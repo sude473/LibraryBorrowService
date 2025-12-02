6edc506db6fff060f08092d5d2c240af731e3b3f
sude473 <sudenurciliz4422@gmail.com>
2025-12-02 10:32:39 +0300

    Update RabbitMQConfig.java

--- src/main/java/com/group12/libraryborrowservice/config/RabbitMQConfig.java
+++ src/main/java/com/group12/libraryborrowservice/config/RabbitMQConfig.java
@@ -3,13 +3,24 @@ package com.group12.libraryborrowservice.config;
 import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+/**
+ * RabbitMQ configuration class.
+ *
+ * This class belongs to the MESSAGING LAYER and defines the queue
+ * that will be used by the application.
+ */
 @Configuration
 public class RabbitMQConfig {
 
-
+/**
+     * Defines a queue named 'borrowQueue'.
+     * This queue is used by BorrowServiceImpl when sending messages.
+     *
+     * @return Queue bean managed by Spring
+     */
     @Bean
     public Queue borrowQueue() {
+      
         return new Queue("borrowQueue", false);
     }
 }
