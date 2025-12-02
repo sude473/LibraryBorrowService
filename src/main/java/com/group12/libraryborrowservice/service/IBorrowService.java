a01032e9823108cb1b7da85a97b1ab0a3837541f
sude473 <sudenurciliz4422@gmail.com>
2025-12-02 10:25:04 +0300

    comment satırı ekledim
    Added Javadoc comments to IBorrowService interface.

--- src/main/java/com/group12/libraryborrowservice/service/IBorrowService.java
+++ src/main/java/com/group12/libraryborrowservice/service/IBorrowService.java
@@ -1,6 +1,18 @@
 package com.group12.libraryborrowservice.service;
-
+/**
+ * Service interface for borrow operations.
+ *
+ * This interface defines the CONTRACT for the borrow service
+ * and is implemented by BorrowServiceImpl.
+ */
 public interface IBorrowService {
-    // Defines the contract for borrowing a book
+    
+    /**
+     * Creates a borrow record for the given user and book.
+     *
+     * @param userId ID of the user who borrows the book
+     * @param bookId ID of the borrowed book
+     * @return true if the operation is successful, false otherwise
+     */
     boolean createBorrowRecord(Long userId, Long bookId);
 }
