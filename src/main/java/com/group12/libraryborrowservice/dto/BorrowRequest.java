bfe079459653b00848e35c4605b9ddaef5c2dda2
sude473 <sudenurciliz4422@gmail.com>
2025-12-02 10:30:17 +0300

    Update BorrowRequest.java

--- src/main/java/com/group12/libraryborrowservice/dto/BorrowRequest.java
+++ src/main/java/com/group12/libraryborrowservice/dto/BorrowRequest.java
@@ -1,11 +1,20 @@
 package com.group12.libraryborrowservice.dto;
 
+/**
+ * Data Transfer Object used to receive borrow requests from the client.
+ *
+ * This class is used as the request body in BorrowController.
+ */
 public class BorrowRequest {
-    private Long userId;
-    private Long bookId;
 
-    // Default constructor is required for JSON serialization
-    public BorrowRequest() {}
+    private Long userId;   // ID of the user
+    private Long bookId;   // ID of the book
+
+    /**
+     * Default constructor is required for JSON deserialization.
+     */
+    public BorrowRequest() {
+    }
 
     public Long getUserId() {
         return userId;
